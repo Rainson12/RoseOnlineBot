3c7b68302e43bb78fae28c3e14642fb8f3f26aad
Rene Steen <rene@steen.dev>
2023-05-26 20:00:18 +0200

    .

---
+++ RoseOnlineBot/Models/Logic/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoseOnlineBot.Models.Logic
+{
+    internal class Skill
+    {
+        public int Id { get; set; }
+        public int CooldownInMilliseconds { get; set; }
+        public DateTime? LastExecution { get; set; }
+        public bool IsOnCooldown => LastExecution != null ? LastExecution.Value.AddMilliseconds(CooldownInMilliseconds) < DateTime.Now : true;
+    }
+}
